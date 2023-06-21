6c629a093bd16d9d6c9a955d6808e19888b7e36f
Tri <trimpham98@gmail.com>
2023-06-21 12:05:12 -0700

    Create the geometry, material, and points and add to scene

--- src/script.js
+++ src/script.js
@@ -17,11 +17,49 @@ const scene = new THREE.Scene();
 /**
  * Galaxy
  */
-const generateGalaxy = () => {
+const parameters = {};
+parameters.count = 1000;
+parameters.size = 0.02;
 
+const generateGalaxy = () => {
+  /**
+   * Geometry
+   */
+  const geometry = new THREE.BufferGeometry();
+
+  const positions = new Float32Array(parameters.count * 3);
+
+  for (let i = 0; i < parameters.count; i++) {
+    const i3 = i * 3;
+
+    positions[i3 + 0] = Math.random();
+    positions[i3 + 1] = Math.random();
+    positions[i3 + 2] = Math.random();
+  }
+
+  geometry.setAttribute(
+    "position",
+    new THREE.BufferAttribute(positions, 3)
+  )
+
+  /**
+   * Material
+   */
+  const material = new THREE.PointsMaterial({
+    size: parameters.size,
+    sizeAttenuation: true,
+    depthWrite: false,
+    blending: THREE.AdditiveBlending
+  });
+
+  /**
+   * Points
+   */
+  const points = new THREE.Points(geometry, material);
+  scene.add(points);
 };
 
-generateGalaxy;
+generateGalaxy();
 
 /**
  * Sizes
