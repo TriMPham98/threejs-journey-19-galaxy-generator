808b88afa57ec82e4237e3f7d28011a700f36bea
Tri <trimpham98@gmail.com>
2023-06-21 11:53:06 -0700

    Remove temp cube and add generateGalaxy function

--- src/script.js
+++ src/script.js
@@ -15,13 +15,13 @@ const canvas = document.querySelector("canvas.webgl");
 const scene = new THREE.Scene();
 
 /**
- * Test cube
+ * Galaxy
  */
-const cube = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial()
-);
-scene.add(cube);
+const generateGalaxy = () => {
+
+};
+
+generateGalaxy;
 
 /**
  * Sizes
